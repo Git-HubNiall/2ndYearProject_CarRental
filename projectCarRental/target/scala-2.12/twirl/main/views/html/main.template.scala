63cac1827019740e67b2c7e14f6e64d898597047
wdd <joythompson12@gmail.com>
2019-03-01 18:28:04 +0000

    need to add pictures and also need to give each page individual html

--- projectCarRental/target/scala-2.12/twirl/main/views/html/main.template.scala
+++ projectCarRental/target/scala-2.12/twirl/main/views/html/main.template.scala
@@ -38,55 +38,71 @@ Seq[Any](format.raw/*2.1*/("""<!DOCTYPE html>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<style>
 	    .footer """),format.raw/*9.14*/("""{"""),format.raw/*9.15*/(""" """),format.raw/*9.16*/("""font-weight: bold; color: white; background-color: black; """),format.raw/*9.74*/("""}"""),format.raw/*9.75*/("""
-	    """),format.raw/*10.6*/(""".sidecol """),format.raw/*10.15*/("""{"""),format.raw/*10.16*/(""" """),format.raw/*10.17*/("""font-weight: bold; background-color: steelblue; """),format.raw/*10.65*/("""}"""),format.raw/*10.66*/("""
+	    """),format.raw/*10.6*/(""".sidecol """),format.raw/*10.15*/("""{"""),format.raw/*10.16*/(""" """),format.raw/*10.17*/("""font-weight: bold; background-color:teal; """),format.raw/*10.59*/("""}"""),format.raw/*10.60*/("""
 	    """),format.raw/*11.6*/(""".sidecol h2 """),format.raw/*11.18*/("""{"""),format.raw/*11.19*/(""" """),format.raw/*11.20*/("""font-weight: bold; """),format.raw/*11.39*/("""}"""),format.raw/*11.40*/("""
 	    """),format.raw/*12.6*/(""".midcol h1 """),format.raw/*12.17*/("""{"""),format.raw/*12.18*/(""" """),format.raw/*12.19*/("""text-align: center; font-weight: bold; """),format.raw/*12.58*/("""}"""),format.raw/*12.59*/("""
 	    """),format.raw/*13.6*/(""".midcol h3 """),format.raw/*13.17*/("""{"""),format.raw/*13.18*/(""" """),format.raw/*13.19*/("""text-align: center; color: steelblue; """),format.raw/*13.57*/("""}"""),format.raw/*13.58*/("""
 	    """),format.raw/*14.6*/(""".redtext """),format.raw/*14.15*/("""{"""),format.raw/*14.16*/(""" """),format.raw/*14.17*/("""color: red; """),format.raw/*14.29*/("""}"""),format.raw/*14.30*/("""
-	"""),format.raw/*15.2*/("""</style>
-	<title>OnlineCarRental """),_display_(/*16.26*/pagename),format.raw/*16.34*/("""</title>
+		"""),format.raw/*15.3*/(""".sidecol input """),format.raw/*15.18*/("""{"""),format.raw/*15.19*/("""width: 100%;"""),format.raw/*15.31*/("""}"""),format.raw/*15.32*/("""
+		"""),format.raw/*16.3*/(""".pickUp """),format.raw/*16.11*/("""{"""),format.raw/*16.12*/("""float: left;"""),format.raw/*16.24*/("""}"""),format.raw/*16.25*/("""
+		"""),format.raw/*17.3*/(""".dropOff """),format.raw/*17.12*/("""{"""),format.raw/*17.13*/("""float: right;"""),format.raw/*17.26*/("""}"""),format.raw/*17.27*/("""
+		"""),format.raw/*18.3*/(""".sidecol form """),format.raw/*18.17*/("""{"""),format.raw/*18.18*/("""width: 50%;"""),format.raw/*18.29*/("""}"""),format.raw/*18.30*/("""
+	"""),format.raw/*19.2*/("""</style>
+	<title>OnlineCarRental """),_display_(/*20.26*/pagename),format.raw/*20.34*/("""</title>
     </head>
     <body>
+	<link rel="header" type="image/jpg" href=""""),_display_(/*23.45*/routes/*23.51*/.Assets.versioned("images/carheader.jpg")),format.raw/*23.92*/("""">
 	<nav class="navbar navbar-inverse">
 	    <div class="container-fluid">
 		<div class="navbar-header">
 		    <a class="navbar-brand" href="#">OnlineCarRental</a>
 		</div>
 		<ul class="nav navbar-nav">
-            <li """),_display_(/*25.18*/if(pagename == "Home")/*25.40*/ {_display_(Seq[Any](format.raw/*25.42*/("""class="active"""")))}),format.raw/*25.57*/("""><a href=""""),_display_(/*25.68*/routes/*25.74*/.HomeController.index()),format.raw/*25.97*/("""">Home</a></li>
-		    <li """),_display_(/*26.12*/if(pagename == "On Sale")/*26.37*/ {_display_(Seq[Any](format.raw/*26.39*/("""class="active"""")))}),format.raw/*26.54*/("""><a href=""""),_display_(/*26.65*/routes/*26.71*/.HomeController.onsale()),format.raw/*26.95*/("""">On Sale</a></li>
-			<li """),_display_(/*27.9*/if(pagename == "About")/*27.32*/ {_display_(Seq[Any](format.raw/*27.34*/("""class="active"""")))}),format.raw/*27.49*/("""><a href=""""),_display_(/*27.60*/routes/*27.66*/.HomeController.about()),format.raw/*27.89*/("""">About us</a></li>
+            <li """),_display_(/*30.18*/if(pagename == "Home")/*30.40*/ {_display_(Seq[Any](format.raw/*30.42*/("""class="active"""")))}),format.raw/*30.57*/("""><a href=""""),_display_(/*30.68*/routes/*30.74*/.HomeController.index()),format.raw/*30.97*/("""">Home</a></li>
+		    <li """),_display_(/*31.12*/if(pagename == "On Sale")/*31.37*/ {_display_(Seq[Any](format.raw/*31.39*/("""class="active"""")))}),format.raw/*31.54*/("""><a href=""""),_display_(/*31.65*/routes/*31.71*/.HomeController.onsale()),format.raw/*31.95*/("""">On Sale</a></li>
+			<li """),_display_(/*32.9*/if(pagename == "About")/*32.32*/ {_display_(Seq[Any](format.raw/*32.34*/("""class="active"""")))}),format.raw/*32.49*/("""><a href=""""),_display_(/*32.60*/routes/*32.66*/.HomeController.about()),format.raw/*32.89*/("""">About us</a></li>
 			
-			<li """),_display_(/*29.9*/if(pagename == "Log In")/*29.33*/ {_display_(Seq[Any](format.raw/*29.35*/("""class="active"""")))}),format.raw/*29.50*/(""">
-				"""),_display_(/*30.6*/if(user != null)/*30.22*/ {_display_(Seq[Any](format.raw/*30.24*/("""
-					"""),format.raw/*31.6*/("""<a href=""""),_display_(/*31.16*/routes/*31.22*/.LoginController.logout()),format.raw/*31.47*/("""">Log Out</a>
-				""")))}/*32.7*/else/*32.12*/{_display_(Seq[Any](format.raw/*32.13*/("""
-					"""),format.raw/*33.6*/("""<a href=""""),_display_(/*33.16*/routes/*33.22*/.LoginController.login()),format.raw/*33.46*/("""">Log In</a>
+			<li """),_display_(/*34.9*/if(pagename == "Log In")/*34.33*/ {_display_(Seq[Any](format.raw/*34.35*/("""class="active"""")))}),format.raw/*34.50*/(""">
+				"""),_display_(/*35.6*/if(user != null)/*35.22*/ {_display_(Seq[Any](format.raw/*35.24*/("""
+					"""),format.raw/*36.6*/("""<a href=""""),_display_(/*36.16*/routes/*36.22*/.LoginController.logout()),format.raw/*36.47*/("""">Log Out</a>
+				""")))}/*37.7*/else/*37.12*/{_display_(Seq[Any](format.raw/*37.13*/("""
+					"""),format.raw/*38.6*/("""<a href=""""),_display_(/*38.16*/routes/*38.22*/.LoginController.login()),format.raw/*38.46*/("""">Log In</a>
+			
 			</li>
-""")))}),format.raw/*35.2*/("""
-		"""),format.raw/*36.3*/("""</ul>
+""")))}),format.raw/*41.2*/("""			
+		"""),format.raw/*42.3*/("""</ul>
 	    </div>
 	</nav>
-
+	<div>
+		<div class="col-sm-12">
+				<div class="midcol">
+					"""),_display_(/*48.7*/content),format.raw/*48.14*/("""
+					
+				"""),format.raw/*50.5*/("""</div>
+		</div>
+	</div>
 	<div class="container-fluid">
 	    <div class="row">
-		<div class="col-sm-2">
+		<div class="col-sm-6">
 		    <div class="well sidecol">
-			<h2>Car Rental</h2>
-			<h3>Rent cars by booking online</h3>
-			<p>choose cars to rent from, <br> 5 Seaters, <br> 7 Seaters, <br> Mini buses</p>
+					<input type="text" placeholder="Search..">
+					<form action="/action_page.php">
+						Pick up date:
+						<input type="date" name="pickUp">
+						<input type="submit">
+					</form>
+	
+					<form action="/action_page.php">
+						Drop off date:
+						<input type="date" name="dropOff">
+						<input type="submit">
+					</form>	
 		    </div>
 		</div>
-		<div class="col-sm-8">
-		    <div class="midcol">
-			"""),_display_(/*51.5*/content),format.raw/*51.12*/("""
-		    """),format.raw/*52.7*/("""</div>
-		</div>
-		<div class="col-sm-2">
+		
+		<div class="col-sm-6">
 		    <div class="well sidecol">
-			<h2></h2>
-			<h3></h3>
-			<p></p>
+			
 		    </div>
 		</div>
 	    </div>
@@ -98,7 +114,7 @@ Seq[Any](format.raw/*2.1*/("""<!DOCTYPE html>
 		</div>
 	    </div>
 	</div>
-	<script src =""""),_display_(/*70.17*/routes/*70.23*/.Assets.versioned("javascripts/main.js")),format.raw/*70.63*/(""""></script>
+	<script src =""""),_display_(/*86.17*/routes/*86.23*/.Assets.versioned("javascripts/main.js")),format.raw/*86.63*/(""""></script>
     </body>
 </html>
 
@@ -118,11 +134,11 @@ Seq[Any](format.raw/*2.1*/("""<!DOCTYPE html>
 
               /*
                   -- GENERATED --
-                  DATE: Tue Feb 26 21:42:43 GMT 2019
+                  DATE: Fri Mar 01 18:10:26 GMT 2019
                   SOURCE: /home/wdd/year2Project/projectCarRental/app/views/main.scala.html
-                  HASH: 72df949e951efa4d7ad8bc4bcd3c818f38aa0f0d
-                  MATRIX: 970->1|1119->57|1401->312|1429->313|1457->314|1542->372|1570->373|1603->379|1640->388|1669->389|1698->390|1774->438|1803->439|1836->445|1876->457|1905->458|1934->459|1981->478|2010->479|2043->485|2082->496|2111->497|2140->498|2207->537|2236->538|2269->544|2308->555|2337->556|2366->557|2432->595|2461->596|2494->602|2531->611|2560->612|2589->613|2629->625|2658->626|2687->628|2748->662|2777->670|3053->919|3084->941|3124->943|3170->958|3208->969|3223->975|3267->998|3321->1025|3355->1050|3395->1052|3441->1067|3479->1078|3494->1084|3539->1108|3592->1135|3624->1158|3664->1160|3710->1175|3748->1186|3763->1192|3807->1215|3865->1247|3898->1271|3938->1273|3984->1288|4017->1295|4042->1311|4082->1313|4115->1319|4152->1329|4167->1335|4213->1360|4250->1380|4263->1385|4302->1386|4335->1392|4372->1402|4387->1408|4432->1432|4485->1455|4515->1458|4905->1822|4933->1829|4967->1836|5304->2146|5319->2152|5380->2192
-                  LINES: 28->1|33->2|40->9|40->9|40->9|40->9|40->9|41->10|41->10|41->10|41->10|41->10|41->10|42->11|42->11|42->11|42->11|42->11|42->11|43->12|43->12|43->12|43->12|43->12|43->12|44->13|44->13|44->13|44->13|44->13|44->13|45->14|45->14|45->14|45->14|45->14|45->14|46->15|47->16|47->16|56->25|56->25|56->25|56->25|56->25|56->25|56->25|57->26|57->26|57->26|57->26|57->26|57->26|57->26|58->27|58->27|58->27|58->27|58->27|58->27|58->27|60->29|60->29|60->29|60->29|61->30|61->30|61->30|62->31|62->31|62->31|62->31|63->32|63->32|63->32|64->33|64->33|64->33|64->33|66->35|67->36|82->51|82->51|83->52|101->70|101->70|101->70
+                  HASH: aa080756b89a7630181bc37cf87612980b5776af
+                  MATRIX: 970->1|1119->57|1401->312|1429->313|1457->314|1542->372|1570->373|1603->379|1640->388|1669->389|1698->390|1768->432|1797->433|1830->439|1870->451|1899->452|1928->453|1975->472|2004->473|2037->479|2076->490|2105->491|2134->492|2201->531|2230->532|2263->538|2302->549|2331->550|2360->551|2426->589|2455->590|2488->596|2525->605|2554->606|2583->607|2623->619|2652->620|2682->623|2725->638|2754->639|2794->651|2823->652|2853->655|2889->663|2918->664|2958->676|2987->677|3017->680|3054->689|3083->690|3124->703|3153->704|3183->707|3225->721|3254->722|3293->733|3322->734|3351->736|3412->770|3441->778|3544->854|3559->860|3621->901|3868->1121|3899->1143|3939->1145|3985->1160|4023->1171|4038->1177|4082->1200|4136->1227|4170->1252|4210->1254|4256->1269|4294->1280|4309->1286|4354->1310|4407->1337|4439->1360|4479->1362|4525->1377|4563->1388|4578->1394|4622->1417|4680->1449|4713->1473|4753->1475|4799->1490|4832->1497|4857->1513|4897->1515|4930->1521|4967->1531|4982->1537|5028->1562|5065->1582|5078->1587|5117->1588|5150->1594|5187->1604|5202->1610|5247->1634|5304->1661|5337->1667|5453->1757|5481->1764|5519->1775|6299->2528|6314->2534|6375->2574
+                  LINES: 28->1|33->2|40->9|40->9|40->9|40->9|40->9|41->10|41->10|41->10|41->10|41->10|41->10|42->11|42->11|42->11|42->11|42->11|42->11|43->12|43->12|43->12|43->12|43->12|43->12|44->13|44->13|44->13|44->13|44->13|44->13|45->14|45->14|45->14|45->14|45->14|45->14|46->15|46->15|46->15|46->15|46->15|47->16|47->16|47->16|47->16|47->16|48->17|48->17|48->17|48->17|48->17|49->18|49->18|49->18|49->18|49->18|50->19|51->20|51->20|54->23|54->23|54->23|61->30|61->30|61->30|61->30|61->30|61->30|61->30|62->31|62->31|62->31|62->31|62->31|62->31|62->31|63->32|63->32|63->32|63->32|63->32|63->32|63->32|65->34|65->34|65->34|65->34|66->35|66->35|66->35|67->36|67->36|67->36|67->36|68->37|68->37|68->37|69->38|69->38|69->38|69->38|72->41|73->42|79->48|79->48|81->50|117->86|117->86|117->86
                   -- GENERATED --
               */
           
