140c22164b3a1c5578104686089aa63aa23bf0a7
Niall <n.oreilly.itt@gmail.com>
2019-03-06 21:51:27 +0000

    Removed old files

--- projectCarRental/conf/evolutions/default/3.sql
+++
@@ -1,4 +0,0 @@
-# --- !Ups
-insert into user (email,name,password,role) values ( 'admin@ocr.com', 'Alice Admin', 'password', 'admin' );
-insert into user (email,name,password,role) values ( 'manager@ocr.com', 'Bob Manager', 'password', 'manager' );
-insert into user (email,name,password,role) values ( 'customer@ocr.com', 'Charlie Customer', 'password', 'customer' );
